9b517c62a8db6d6e7c2117e4596119829e6a6fee
Ashutosh Chandra <97390449+Ashutosh-Chandra5@users.noreply.github.com>
2024-08-03 20:52:49 +0530

    Update frontend.sh

--- frontend.sh
+++ frontend.sh
@@ -2,5 +2,5 @@
 sudo apt update -y
 sudo apt install docker.io -y
 sudo systemctl start docker
-sudo docker run -d -p 80:80 your-dockerhub-username/frontend:latest
+sudo docker run -d -p 80:80 kubeashukube/frontend:latest
 
